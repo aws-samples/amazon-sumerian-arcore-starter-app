25890852fc95b0b7163e4ec6311a6f66e54b3065
Mike Starr <starrm@amazon.com>
2018-02-06 14:54:36 -0800

    Adding SumerianARCoreStarter Android Studio project.

---
+++ SumerianARCoreStarter/app/src/main/java/com/amazon/sumerianarcorestarter/MainActivity.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * This code has been modified. Portions copyright 2018 Amazon.com, Inc. or its affiliates. Please see LICENSE.txt for applicable license terms and NOTICE.txt for applicable notices.
+ */
+
+package com.amazon.sumerianarcorestarter;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Session;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MainActivity extends AppCompatActivity implements GLSurfaceView.Renderer {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String SCENE_URL = "https://d1550wa51vq95s.cloudfront.net/3d19ea8069a94904849e8edeabe3ada0.scene/?arMode=true";
+
+    private GLSurfaceView mSurfaceView;
+    private Session mSession;
+    private SumerianConnector mSumerianConnector;
+
+    private final BackgroundRenderer mBackgroundRenderer = new BackgroundRenderer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSurfaceView = findViewById(R.id.gl_surface_view);
+
+        // Set up renderer.
+        mSurfaceView.setPreserveEGLContextOnPause(true);
+        mSurfaceView.setEGLContextClientVersion(2);
+        mSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0); // Alpha used for plane blending.
+        mSurfaceView.setRenderer(this);
+        mSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+
+        try {
+            mSession = new Session(/* context= */ this);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        final WebView webView = findViewById(R.id.activity_main_webview);
+        mSumerianConnector = new SumerianConnector(webView, mSession, mSurfaceView);
+
+        // Create config and check if supported.
+        Config config = new Config(mSession);
+        config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+        if (!mSession.isSupported(config)) {
+            throw new RuntimeException("This device does not support AR");
+        }
+
+        mSession.configure(config);
+        mSumerianConnector.loadUrl(SCENE_URL);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            // Standard Android full-screen functionality.
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+        // permission on Android M and above, now is a good time to ask the user for it.
+        if (CameraPermissionHelper.hasCameraPermission(this)) {
+            if (mSession != null) {
+                // Note that order matters - see the note in onPause(), the reverse applies here.
+                mSession.resume();
+            }
+            mSurfaceView.onResume();
+        } else {
+            CameraPermissionHelper.requestCameraPermission(this);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Note that the order matters - GLSurfaceView is paused first so that it does not try
+        // to query the session. If Session is paused before GLSurfaceView, GLSurfaceView may
+        // still call mSession.update() and get a SessionPausedException.
+        mSurfaceView.onPause();
+        if (mSession != null) {
+            mSession.pause();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this,
+                    "Camera permission is needed to run this application", Toast.LENGTH_LONG).show();
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this);
+            }
+            finish();
+        }
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+        // Create the texture and pass it to ARCore session to be filled during update().
+        mBackgroundRenderer.createOnGlThread(/*context=*/this);
+        mSession.setCameraTextureName(mBackgroundRenderer.getTextureId());
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+        mSession.setDisplayGeometry(getSystemService(WindowManager.class).getDefaultDisplay().getRotation(), width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        // Clear screen to notify driver it should not load any pixels from previous frame.
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        if (mSession == null) {
+            return;
+        }
+
+        try {
+            // Obtain the current frame from ARSession. When the configuration is set to
+            // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
+            // camera framerate.
+            final Frame frame = mSession.update();
+
+            // Draw background.
+            mBackgroundRenderer.draw(frame);
+            mSumerianConnector.update();
+        } catch (Throwable t) {
+            // Avoid crashing the application due to unhandled exceptions.
+            Log.e(TAG, "Exception on the OpenGL thread", t);
+        }
+    }
+}
---
+++ SumerianARCoreStarter/app/src/main/java/com/amazon/sumerianarcorestarter/SumerianConnector.java
@@ -0,0 +1,174 @@
+// Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+// http://aws.amazon.com/apache2.0/
+// or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+//
+package com.amazon.sumerianarcorestarter;
+
+import android.opengl.GLSurfaceView;
+import android.os.Handler;
+import android.os.Looper;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Camera;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.LightEstimate;
+import com.google.ar.core.Pose;
+import com.google.ar.core.Session;
+import com.google.ar.core.Trackable;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+
+class SumerianConnector {
+
+    private WebView mWebView;
+    private Session mSession;
+    private GLSurfaceView mSurfaceView;
+
+    private final float[] mViewMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+
+    SumerianConnector(WebView webView, Session session, GLSurfaceView surfaceView) {
+        mWebView = webView;
+        mSession = session;
+        mSurfaceView = surfaceView;
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.addJavascriptInterface(new BridgeInterface(), "Android");
+
+        this.mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                view.setBackgroundColor(0x00000000);
+            }
+        });
+    }
+
+    void loadUrl(String url) {
+        mWebView.loadUrl(url);
+    }
+
+    void update() {
+        final Frame frame = mSession.update();
+        final Camera camera = frame.getCamera();
+
+        if (camera.getTrackingState() == Trackable.TrackingState.PAUSED) {
+            return;
+        }
+
+        camera.getViewMatrix(mViewMatrix, 0);
+        camera.getProjectionMatrix(mProjectionMatrix, 0, 0.02f, 20.0f);
+
+        final String cameraUpdateString = "ARCoreBridge.viewProjectionMatrixUpdate('" + serializeArray(mViewMatrix) +"', '"+ serializeArray(mProjectionMatrix) + "');";
+        evaluateWebViewJavascript(cameraUpdateString);
+
+        HashMap<String, float[]> anchorMap = new HashMap<>();
+
+        for (Anchor anchor : mSession.getAllAnchors()) {
+            if (anchor.getTrackingState() != Trackable.TrackingState.TRACKING) {
+                continue;
+            }
+
+            final float[] anchorPoseMatrix = new float[16];
+            anchor.getPose().toMatrix(anchorPoseMatrix, 0);
+            anchorMap.put(String.valueOf(anchor.hashCode()), anchorPoseMatrix);
+        }
+
+        if (anchorMap.size() > 0) {
+            JSONObject jsonAnchors = new JSONObject(anchorMap);
+            final String anchorUpdateScript = "ARCoreBridge.anchorTransformUpdate('" + jsonAnchors.toString() + "');";
+            evaluateWebViewJavascript(anchorUpdateScript);
+        }
+
+        if (frame.getLightEstimate().getState() != LightEstimate.State.NOT_VALID) {
+            final String lightEstimateUpdateScript = "ARCoreBridge.lightingEstimateUpdate(" + String.valueOf(frame.getLightEstimate().getPixelIntensity()) + ");";
+            evaluateWebViewJavascript(lightEstimateUpdateScript);
+        }
+    }
+
+    private String serializeArray(float[] array) {
+        try {
+            JSONArray jsonArray = new JSONArray(array);
+            return jsonArray.toString();
+        } catch (JSONException e) {
+            return "";
+        }
+    }
+
+    private void evaluateWebViewJavascript(final String scriptString) {
+        final Handler mainHandler = new Handler(Looper.getMainLooper());
+        final Runnable webViewUpdate = new Runnable() {
+            @Override
+            public void run() {
+                mWebView.evaluateJavascript(scriptString, null);
+            }
+        };
+
+        mainHandler.postAtFrontOfQueue(webViewUpdate);
+    }
+
+    private class BridgeInterface {
+
+        private float[] mHitTestResultPose = new float[16];
+
+        @JavascriptInterface
+        public void requestHitTest(final String requestId, final float screenX, final float screenY) {
+            if (requestId == null) {
+                return;
+            }
+
+            mSurfaceView.queueEvent(new Runnable() {
+                @Override
+                public void run() {
+                    final float hitTestX = screenX * mWebView.getWidth();
+                    final float hitTestY = screenY * mWebView.getHeight();
+
+                    List<HitResult> hitTestResults = mSession.update().hitTest(hitTestX, hitTestY);
+
+                    final String scriptString;
+
+                    if (hitTestResults.size() > 0) {
+                        hitTestResults.get(0).getHitPose().toMatrix(mHitTestResultPose, 0);
+                        scriptString = "ARCoreBridge.hitTestResponse('" + requestId + "', '" + serializeArray(mHitTestResultPose) + "');";
+
+                    } else {
+                        scriptString = "ARCoreBridge.hitTestResponse('" + requestId + "', null);";
+                    }
+
+                    evaluateWebViewJavascript(scriptString);
+                }
+            });
+        }
+
+        @JavascriptInterface
+        public void registerAnchor(final String requestId, final float[] matrix) {
+            if (requestId == null || matrix == null) {
+                return;
+            }
+
+            mSurfaceView.queueEvent(new Runnable() {
+                @Override
+                public void run() {
+                    Pose anchorPose = Pose.makeTranslation(matrix[12], matrix[13], matrix[14]);
+                    Anchor anchor = mSession.createAnchor(anchorPose);
+
+                    final String scriptString = "ARCoreBridge.registerAnchorResponse('" + requestId + "', '" + String.valueOf(anchor.hashCode()) + "');";
+                    evaluateWebViewJavascript(scriptString);
+                }
+            });
+        }
+    }
+}
